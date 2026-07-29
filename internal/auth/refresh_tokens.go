8d7f6173305a1cf7839273ce87702eea414116f6
Filz0r <filipefig26@gmail.com>
2026-07-29 19:11:07 +0100

    chore: removed dead code

--- internal/auth/refresh_tokens.go
+++ internal/auth/refresh_tokens.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"crypto/rand"
-
 	"encoding/hex"
 )
 
@@ -12,20 +11,3 @@ func MakeRefreshToken() string {
 	encoded := hex.EncodeToString(key)
 	return encoded
 }
-
-//func ValidateRefreshToken(tokenString string, db *database.Queries, ctx context.Context) (database.RefreshToken, bool) {
-//	token, err := db.GetRefreshToken(ctx, tokenString)
-//	if err != nil {
-//		fmt.Printf("Error validating refresh token: %v", err)
-//		return database.RefreshToken{}, false
-//	}
-//
-//	if token.RevokedAt.Valid {
-//		return database.RefreshToken{}, false
-//	}
-//	if time.Now().After(token.ExpiresAt) {
-//		return database.RefreshToken{}, false
-//	}
-//
-//	return token, true
-//}
