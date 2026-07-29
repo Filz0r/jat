35461a6f02a5fd2af1cf6a56d4081215d36effe2
Filz0r <filipefig26@gmail.com>
2026-07-29 19:11:47 +0100

    feat: added methods for getting users by email and id, checking if a user is admin by id, and one to get all users

--- internal/services/users.go
+++ internal/services/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/filz0r/jat/internal/auth"
 	"github.com/filz0r/jat/internal/database"
+	"github.com/google/uuid"
 )
 
 func (sm *ServiceManager) CreateUser(user database.User) (database.User, error) {
@@ -36,4 +37,43 @@ func (sm *ServiceManager) UpdateUser(user database.User) (database.User, error) 
 	}
 	result := sm.db.Save(&user)
 	return user, result.Error
+}
+
+func (sm *ServiceManager) GetUserByEmail(email string) (database.User, error) {
+	if sm.db == nil {
+		return database.User{}, errors.New("database not initialized")
+	}
+	user := database.User{}
+	result := sm.db.Where("email = ?", email).First(&user)
+	return user, result.Error
+}
+
+func (sm *ServiceManager) GetUserByID(id uuid.UUID) (database.User, error) {
+	if sm.db == nil {
+		return database.User{}, errors.New("database not initialized")
+	}
+	user := database.User{}
+	result := sm.db.Where("id = ?", id).First(&user)
+	return user, result.Error
+}
+
+func (sm *ServiceManager) IsUserAdmin(id uuid.UUID) bool {
+	if sm.db == nil {
+		return false
+	}
+	user := database.User{}
+	result := sm.db.Where("id = ?", id).First(&user)
+	if result.Error != nil {
+		return false
+	}
+	return user.IsAdmin
+}
+
+func (sm *ServiceManager) GetAllUsers() ([]database.User, error) {
+	if sm.db == nil {
+		return []database.User{}, errors.New("database not initialized")
+	}
+	var users []database.User
+	result := sm.db.Find(&users)
+	return users, result.Error
 }
