719c7f58801c1c2d631f5052da8b278dd43dd213
Filz0r <filipefig26@gmail.com>
2026-08-10 12:39:40 +0100

    improved version info

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,38 @@
 package version
 
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+)
+
 var (
 	Version   = "undefined"
 	Commit    = "undefined"
 	BuildDate = "undefined"
 )
+
+func GoInstallVersion() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			return info.Main.Version
+		}
+	}
+	return ""
+}
+
+func EffectiveVersion() string {
+	if v := GoInstallVersion(); v != "" {
+		return v
+	}
+	return Version
+}
+
+func Info() string {
+	goVersion := runtime.Version()
+	os := runtime.GOOS
+	arch := runtime.GOARCH
+	return fmt.Sprintf("%sVERSION LOG%s\nBuild Version: %s\nEffective Version: %s\nCommit: %s\nBuild Date: %s\nGo: %s OS: %s Arch: %s",
+		"\x1b[35m", "\x1b[0m", Version,
+		EffectiveVersion(), Commit, BuildDate, goVersion, os, arch)
+}
