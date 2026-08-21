4239f028de5e095537a471804075b02019aa9966
Filz0r <filipefig26@gmail.com>
2026-08-21 12:26:27 +0100

    feat: function to get badge colors for application statuses based on kind

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -5,3 +5,22 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function getColorFromKind(kind: string): string {
+	switch (kind) {
+		case 'applied':
+			return 'bg-orange-500 text-black';
+		case 'rejected':
+			return 'bg-red-500 text-black';
+		case 'ghosted':
+			return 'bg-red-600 text-black';
+		case 'interviewed':
+			return 'bg-primary text-white';
+		case 'irrelevant':
+			return 'bg-muted text-ghost';
+		case 'accepted':
+			return 'bg-green-500 text-green-100';
+		default:
+			return 'bg-background text-white';
+	}
+}
