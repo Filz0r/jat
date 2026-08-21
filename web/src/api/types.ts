ca3300579723a4895fd0d81809384782e5ef4d12
Filz0r <filipefig26@gmail.com>
2026-08-21 19:01:01 +0100

    added a more dev friendly type for api responses

--- web/src/api/types.ts
+++ web/src/api/types.ts
@@ -3,3 +3,5 @@ import type { components } from './gen-spec.ts';
 export type JobApplication = components['schemas']['api.applicationResponse'];
 export type JobApplicationNote = components['schemas']['api.noteData'];
 export type JobApplicationHistory = components['schemas']['api.applicationStatusHistoryResponse'];
+
+export type APIResponse = components['schemas']['api.apiResponse'];
