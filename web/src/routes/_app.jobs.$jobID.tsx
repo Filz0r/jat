082627752b36aca0598790e6bd0ce5355fe89607
Filz0r <filipefig26@gmail.com>
2026-08-22 11:43:56 +0100

    feat: implemented the frontend page for rendering a job application

--- web/src/routes/_app.jobs.$jobID.tsx
+++ web/src/routes/_app.jobs.$jobID.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { apiClient } from '#/api/client.ts';
+import { Skeleton } from '#/components/ui/skeleton.tsx';
+import JobApplicationRenderer from '#/components/entity-renderers/job-application-renderer.tsx';
 
 export const Route = createFileRoute('/_app/jobs/$jobID')({
 	component: RouteComponent,
@@ -8,7 +11,25 @@ function RouteComponent() {
 	const { jobID } = Route.useParams();
 	const parsedID = parseInt(jobID);
 
-	console.log(parsedID);
+	if (isNaN(parsedID)) {
+		return <div> Error fetching job data</div>;
+	}
 
-	return <div>Hello "/jobs/{jobID}"!</div>;
+	const { data, isLoading, isError, error } = apiClient.useQuery('get', '/jobs/{jobID}', {
+		params: {
+			path: {
+				jobID: parsedID,
+			},
+		},
+	});
+
+	return (
+		<div className="mx-4">
+			{isLoading && <Skeleton className="size-full" />}
+			{isError && (
+				<div className="text-xl text-red-500">Error loading data {error.message}</div>
+			)}
+			{data && data.data && <JobApplicationRenderer data={data.data} />}
+		</div>
+	);
 }
