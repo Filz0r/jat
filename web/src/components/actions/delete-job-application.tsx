575c2edc4ac1a5ab9daf711dc043d0b20b8f75ac
Filz0r <filipefig26@gmail.com>
2026-08-22 11:42:35 +0100

    feat: added a reusable component that allows to delete a job application

---
+++ web/src/components/actions/delete-job-application.tsx
@@ -0,0 +1,59 @@
+import type { APIResponse } from '#/api/types.ts';
+import { useQueryClient } from '@tanstack/react-query';
+import ModularDeleteEntityDialog from '#/components/dialogs/modular-delete-entity-dialog.tsx';
+import { api } from '#/api/client.ts';
+import { Button } from '#/components/ui/button.tsx';
+import { Trash } from 'lucide-react';
+import { toast } from '#/components/ui/toast.tsx';
+import { useNavigate } from '@tanstack/react-router';
+
+interface DeleteJobApplicationProps {
+	jobID: number;
+	navigateToJobs?: boolean;
+}
+
+export default function DeleteJobApplication({
+	jobID,
+	navigateToJobs = false,
+}: DeleteJobApplicationProps) {
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
+	return (
+		<ModularDeleteEntityDialog
+			message="You are deleting this job application, this is only reversible by administrative users, are you sure?"
+			title={`Delete job with id of ${jobID}`}
+			onClose={async () => {
+				await toast.promise(
+					Promise.all([
+						api.DELETE('/jobs/{jobID}', {
+							params: {
+								path: {
+									jobID,
+								},
+							},
+						}),
+					]),
+					{
+						loading: `Deleting job with id ${jobID}...`,
+						success: `Deleted job with id ${jobID}`,
+						error: (response: APIResponse) =>
+							`Error deleting note: ${response.message}`,
+					},
+				);
+				await queryClient.refetchQueries({
+					queryKey: ['get', '/jobs'],
+				});
+				if (navigateToJobs) {
+					await navigate({ to: '/jobs', replace: true });
+				}
+			}}
+			triggerButton={
+				<Button className="rounded-3xl p-3" size="icon-sm" variant="destructive">
+					<Trash />
+					<span className="sr-only">Delete this data</span>
+				</Button>
+			}
+			deleteButtonMessage="Delete Job Application"
+		/>
+	);
+}
