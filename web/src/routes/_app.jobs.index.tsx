9ea28ee09da6329005a040911791ad56be2c2ca5
Filz0r <filipefig26@gmail.com>
2026-08-22 11:44:17 +0100

    feat: implemented the frontend page for rendering all job applications with most of the actions included

--- web/src/routes/_app.jobs.index.tsx
+++ web/src/routes/_app.jobs.index.tsx
@@ -1,37 +1,188 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import type { DataTableFeatures } from '#/components/data-table/table-features.ts';
+import type { JobApplication } from '#/api/types.ts';
 
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
 import { apiClient } from '#/api/client.ts';
 import { DataTable } from '#/components/data-table';
 import { createColumnHelper } from '@tanstack/react-table';
-import type { DataTableFeatures } from '#/components/data-table/table-features.ts';
-import type { JobApplication } from '#/api/types.ts';
 import { Button } from '#/components/ui/button.tsx';
-import { Plus } from 'lucide-react';
+import { ArrowUpDown, ArrowUpRightIcon, Eye, Plus } from 'lucide-react';
+import { getColorFromKind } from '#/lib/utils.ts';
+import { Badge } from '#/components/ui/badge.tsx';
+import UpdateJobStatusForm from '#/components/forms/update-job-status-form.tsx';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '#/components/ui/tooltip.tsx';
+import JobNoteForm from '#/components/forms/job-note-form.tsx';
+import DeleteJobApplication from '#/components/actions/delete-job-application.tsx';
 
 export const Route = createFileRoute('/_app/jobs/')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const navigate = useNavigate();
 	const { data, isLoading: isDataLoading, isError } = apiClient.useQuery('get', '/jobs');
 
 	const columnHelper = createColumnHelper<DataTableFeatures, JobApplication>();
-
 	const columns = columnHelper.columns([
 		columnHelper.accessor('id', {
-			header: () => <div className="text-center">ID</div>,
+			header: ({ column }) => (
+				<div className="flex justify-center">
+					<Button
+						variant="ghost"
+						onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+						className="flex cursor-pointer items-center justify-center text-center"
+					>
+						<ArrowUpDown />
+						ID
+					</Button>
+				</div>
+			),
+
+			cell: (cell) => <div className="text-center">{cell.getValue()}</div>,
 		}),
 		columnHelper.accessor('title', {
-			header: 'Title',
-		}),
-		columnHelper.accessor('url', {
-			header: 'URL',
+			header: () => <div className="text-center">Title</div>,
+			cell: (cell) => <div className="text-center">{cell.getValue()}</div>,
 		}),
 		columnHelper.accessor('company.name', {
-			header: 'Company',
+			header: () => <div className="text-center">Company</div>,
+			cell: (cell) => <div className="text-center">{cell.getValue()}</div>,
 		}),
+		columnHelper.accessor('status', {
+			sortFn: (rowA, rowB) => {
+				const a = rowA.original.status.kind;
+				const b = rowB.original.status.kind;
+				return a.localeCompare(b);
+			},
+			header: ({ column }) => (
+				<div className="flex justify-center">
+					<Button
+						variant="ghost"
+						onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+						className="cursor-pointer text-center"
+					>
+						<ArrowUpDown />
+						Status
+					</Button>
+				</div>
+			),
+			cell: (cell) => {
+				const cellData = cell.getValue();
+				return (
+					<div className="flex items-center justify-center">
+						<Badge className={'w-full py-2.5 ' + getColorFromKind(cellData.kind)}>
+							{cellData.status}
+						</Badge>
+					</div>
+				);
+			},
+		}),
+		columnHelper.accessor('url', {
+			header: () => <div className="text-center">URL</div>,
+			cell: (cell) => {
+				const value = cell.getValue();
+				return (
+					<div className="flex items-center justify-center">
+						<Badge
+							className="w-fit py-2.5"
+							render={
+								<a href={value}>
+									Link <ArrowUpRightIcon data-icon="inline-end" />
+								</a>
+							}
+						/>
+					</div>
+				);
+			},
+		}),
+
 		columnHelper.accessor('created_at', {
-			header: 'Created At',
+			header: ({ column }) => (
+				<div className="flex justify-center">
+					<Button
+						variant="ghost"
+						onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+						className="flex cursor-pointer items-center justify-between text-center"
+					>
+						<ArrowUpDown />
+						Created At
+					</Button>
+				</div>
+			),
+			cell: (cell) => {
+				const value = cell.getValue();
+				const converted = new Date(value);
+				return <div className="text-center">{converted.toDateString()}</div>;
+			},
+		}),
+		columnHelper.accessor('updated_at', {
+			header: ({ column }) => (
+				<div className="flex justify-center">
+					<Button
+						variant="ghost"
+						onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+						className="flex cursor-pointer items-center justify-between text-center"
+					>
+						<ArrowUpDown />
+						Last Update
+					</Button>
+				</div>
+			),
+			cell: (cell) => {
+				const value = cell.getValue();
+				const converted = new Date(value);
+				return <div className="text-center">{converted.toDateString()}</div>;
+			},
+		}),
+
+		columnHelper.display({
+			id: 'actions',
+			header: () => <div className="text-center">Actions</div>,
+			cell: ({ row }) => {
+				const cellData = row.original;
+
+				return (
+					<div className="flex justify-center gap-x-2">
+						<TooltipProvider>
+							<Tooltip>
+								<TooltipTrigger
+									render={
+										<Button
+											size="icon-sm"
+											onClick={(e) => {
+												e.preventDefault();
+												e.stopPropagation();
+												navigate({
+													to: '/jobs/$jobID',
+													params: { jobID: cellData.id.toString() },
+												});
+											}}
+										>
+											<Eye />
+											<span className="sr-only">View Job Application</span>
+										</Button>
+									}
+								/>
+								<TooltipContent>
+									<p>View Job Application</p>
+								</TooltipContent>
+							</Tooltip>
+						</TooltipProvider>
+
+						<UpdateJobStatusForm
+							currentStatus={cellData.status.id}
+							jobID={cellData.id}
+						/>
+						<JobNoteForm jobID={cellData.id} />
+						<DeleteJobApplication jobID={cellData.id} />
+					</div>
+				);
+			},
 		}),
 	]);
 
