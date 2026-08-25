3eba30ec24fbd2e34a7e1c79aba89346b8088b8e
Filz0r <filipefig26@gmail.com>
2026-08-25 14:59:54 +0100

    feat: added an action component to archive/delete an application status

---
+++ web/src/components/actions/archive-or-delete-application-status.tsx
@@ -0,0 +1,105 @@
+import type { APIResponse, JobApplicationStatus } from '#/api/types.ts';
+
+import ModularDeleteEntityDialog from '#/components/dialogs/modular-delete-entity-dialog.tsx';
+import { toast } from '#components/ui/toast';
+import { api } from '#/api/client.ts';
+import { useQueryClient } from '@tanstack/react-query';
+import { Button } from '#/components/ui/button.tsx';
+import { Trash } from 'lucide-react';
+import { useNavigate } from '@tanstack/react-router';
+import { useState } from 'react';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '#/components/ui/tooltip.tsx';
+
+interface ArchiveOrDeleteApplicationStatusProps {
+	statusID: number;
+	data: JobApplicationStatus;
+	softDelete?: boolean;
+	inView?: boolean;
+}
+
+export default function ArchiveOrDeleteApplicationStatus({
+	data,
+	statusID,
+	softDelete = false,
+	inView = false,
+}: ArchiveOrDeleteApplicationStatusProps) {
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
+	const [open, setOpen] = useState(false);
+	const message = `You are ${!softDelete ? 'archiving' : 'deleting'} this Application Status. Archiving doesn't remove it from the visible data, while deleting removes it from the visible data and also removes all Job Applications and corresponding Notes and status history from the system, deleted data cannot be restored by you, only by Administrative users. You can always restore archived Application Status, and you don't loose visibility of Job Applications and corresponding Notes of archived Application Status. Are you sure?`;
+
+	return (
+		<ModularDeleteEntityDialog
+			open={open}
+			onOpenChange={setOpen}
+			message={message}
+			title={`${softDelete ? 'Delete' : 'Archive'} note with id of ${data.id}`}
+			onClose={async () => {
+				await toast.promise(
+					Promise.all([
+						api.DELETE('/application_statuses/{statusID}', {
+							params: {
+								path: {
+									statusID,
+								},
+								query: {
+									soft_delete: softDelete,
+								},
+							},
+						}),
+					]),
+					{
+						loading: `${softDelete ? 'Deleting' : 'Archiving'} Application Status with id ${data.id}...`,
+						success: `${softDelete ? 'Deleting' : 'Archiving'} Application Status id ${data.id}`,
+						error: (response: APIResponse) =>
+							`Error ${softDelete ? 'deleting' : 'archiving'} Application Status: ${response.message}`,
+					},
+				);
+
+				await queryClient.refetchQueries({
+					queryKey: ['get', '/application_statuses'],
+				});
+				await queryClient.refetchQueries({
+					queryKey: ['get', '/jobs'],
+				});
+				if (inView) {
+					await navigate({ to: '/application_status' });
+				}
+			}}
+			triggerButton={
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger
+							render={
+								<Button
+									className="rounded-3xl p-3"
+									size="icon-sm"
+									variant="destructive"
+									onClick={() => setOpen(true)}
+								>
+									<Trash />
+									<span className="sr-only">
+										{softDelete ? 'Delete' : 'Archive'} this Application Status
+									</span>
+								</Button>
+							}
+						/>
+						<TooltipContent>
+							<p>
+								{softDelete
+									? 'Delete this Application Status and all matching Job Applications'
+									: 'Archive this Application Status'}
+							</p>
+						</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+			}
+			deleteButtonMessage={`${softDelete ? 'Delete' : 'Archive'} Note`}
+		/>
+	);
+}
