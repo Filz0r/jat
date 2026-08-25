0ec90e15b8f62d598654efc3dc41e5a845239592
Filz0r <filipefig26@gmail.com>
2026-08-25 14:58:41 +0100

    helper type for job application status

--- web/src/api/types.ts
+++ web/src/api/types.ts
@@ -1,6 +1,7 @@
 import type { components } from './gen-spec.ts';
 
 export type JobApplication = components['schemas']['api.applicationResponse'];
+export type JobApplicationStatus = components['schemas']['api.applicationStatusResponse'];
 export type JobApplicationNote = components['schemas']['api.noteData'];
 export type JobApplicationHistory = components['schemas']['api.applicationStatusHistoryResponse'];
 
