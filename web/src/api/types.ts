c6fd050a1bfd80a5809273960a4cb534717ec77e
Filz0r <filipefig26@gmail.com>
2026-09-01 18:48:04 +0100

    feat: helper types

--- web/src/api/types.ts
+++ web/src/api/types.ts
@@ -4,5 +4,7 @@ export type JobApplication = components['schemas']['api.applicationResponse'];
 export type JobApplicationStatus = components['schemas']['api.applicationStatusResponse'];
 export type JobApplicationNote = components['schemas']['api.noteData'];
 export type JobApplicationHistory = components['schemas']['api.applicationStatusHistoryResponse'];
+export type CompanyData = components['schemas']['api.companyResponse'];
+export type CompanyChangeData = components['schemas']['api.companyChangeHistoryResponse'];
 
 export type APIResponse = components['schemas']['api.apiResponse'];
