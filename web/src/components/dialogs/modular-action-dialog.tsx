e9a7e7c480125c82dfd999465ebf48b80e5dccb1
Filz0r <filipefig26@gmail.com>
2026-09-01 18:39:32 +0100

    feat: added an action dialog to replace the components that are being deleted in this commit, previously Dialog was being used, now we are using alert-dialog instead

---
+++ web/src/components/dialogs/modular-action-dialog.tsx
@@ -0,0 +1,75 @@
+import type { ReactElement } from 'react';
+
+import {
+	AlertDialog,
+	AlertDialogContent,
+	AlertDialogTitle,
+	AlertDialogHeader,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogCancel,
+	AlertDialogAction,
+	AlertDialogTrigger,
+} from '#/components/ui/alert-dialog.tsx';
+
+interface ModularActionDialogProps {
+	title: string;
+	message: string;
+	open: boolean;
+	onClose: () => void;
+	onConfirm: () => void;
+	trigger: ReactElement;
+	destructive?: boolean;
+	actionLabel?: string;
+}
+
+export default function ModularActionDialog({
+	title,
+	message,
+	open,
+	onClose,
+	onConfirm,
+	trigger,
+	destructive = false,
+	actionLabel = 'Confirm',
+}: ModularActionDialogProps) {
+	let actionStyles = 'cursor-pointer';
+	if (destructive && actionLabel === 'Confirm') {
+		actionLabel = 'Delete';
+	}
+	if (!destructive) {
+		actionStyles += ' bg-green-500 hover:bg-green-600';
+	}
+	return (
+		<AlertDialog open={open} onOpenChange={onClose}>
+			<AlertDialogTrigger render={trigger} />
+			<AlertDialogContent className="px-0 sm:max-w-sm">
+				<AlertDialogHeader>
+					<AlertDialogTitle className="text-primary w-full border-b px-2 pb-2 text-center">
+						{title}
+					</AlertDialogTitle>
+					<AlertDialogDescription className="mx-2.5 py-1 text-center">
+						<p>{message}</p>
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter className="border-t px-2 pt-3">
+					<AlertDialogCancel
+						variant={!destructive ? 'destructive' : 'secondary'}
+						size="lg"
+						className="cursor-pointer"
+					>
+						Cancel
+					</AlertDialogCancel>
+					<AlertDialogAction
+						className={actionStyles}
+						size="lg"
+						onClick={() => onConfirm()}
+						variant={!destructive ? 'default' : 'destructive'}
+					>
+						{actionLabel}
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
