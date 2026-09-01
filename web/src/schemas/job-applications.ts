fbc62316271855ba66195fd04fc5ae848c4054c5
Filz0r <filipefig26@gmail.com>
2026-09-01 14:32:39 +0100

    fix: since this was an http url it should only accept them, instead of all url kinds

--- web/src/schemas/job-applications.ts
+++ web/src/schemas/job-applications.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const createJobApplicationSchema = z.object({
 	title: z.string('You need to provide a title for this job').min(3).max(100),
-	url: z.url('You need to provide an URL').min(3).max(100),
+	url: z.httpUrl('You need to provide an URL'),
 	createdAt: z.date(),
 	company: z.uint32('You need to select a company, you can also create a new one').min(1),
 	status: z.uint32('You need to select a status').min(1),
