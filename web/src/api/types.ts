6f1de364a19cadb69e7a131d00bf395f0a8306ae
Filz0r <filipefig26@gmail.com>
2026-09-02 16:41:56 +0100

    fix: renaming after underlying type was refactored

--- web/src/api/types.ts
+++ web/src/api/types.ts
@@ -4,7 +4,7 @@ export type User = components['schemas']['api.meUserResponse'];
 
 export type JobApplication = components['schemas']['api.applicationResponse'];
 export type JobApplicationStatus = components['schemas']['api.applicationStatusResponse'];
-export type JobApplicationNote = components['schemas']['api.noteData'];
+export type JobApplicationNote = components['schemas']['api.noteDataResponse'];
 export type JobApplicationHistory = components['schemas']['api.applicationStatusHistoryResponse'];
 export type CompanyData = components['schemas']['api.companyResponse'];
 export type CompanyChangeData = components['schemas']['api.companyChangeHistoryResponse'];
