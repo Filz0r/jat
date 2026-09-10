3f9f700b282d4d6870b3c008a7fe2279e48a7b74
Filz0r <filipefig26@gmail.com>
2026-09-10 14:39:07 +0100

    fix: unifying types for internal stats logic

--- internal/utils/types.go
+++ internal/utils/types.go
@@ -1,6 +1,27 @@
 package utils
 
+import "github.com/filz0r/jat/internal/database"
+
 type SuggestionRecord struct {
 	Label string `json:"label"`
 	Value string `json:"value"`
+}
+
+type CompanyCounts struct {
+	UserCount  int64
+	TotalCount int64
+}
+
+type KindCount struct {
+	Kind  database.ApplicationStatusKind
+	Count int64
+}
+
+type ApplicationStatusKindCounts struct {
+	Applied     int64 `json:"applied" validate:"required"`
+	Ghosted     int64 `json:"ghosted" validate:"required"`
+	Rejected    int64 `json:"rejected" validate:"required"`
+	Accepted    int64 `json:"accepted" validate:"required"`
+	Irrelevant  int64 `json:"irrelevant" validate:"required"`
+	Interviewed int64 `json:"interviewed" validate:"required"`
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,11 +2,6 @@ package utils
 
 import "os"
 
-type CompanyCounts struct {
-	UserCount  int64
-	TotalCount int64
-}
-
 func IsDevMode() bool {
 	return os.Getenv("JAT_DEV") != ""
 }
